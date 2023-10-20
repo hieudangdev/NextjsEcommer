d46135a12d85e4b611a3368ad545ad8bfb0cc634
CapTaindev <computermaytinh232@gmail.com>
2023-10-20 16:44:48 +0700

    add prisma fix navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import Image from 'next/image';
 import Navbar from './components/Navbar';
 
 export default function Home() {
-  return (
-    <div className="px-5 max-w-5xl mx-auto">
-      <Navbar />
-    </div>
-  );
+   return (
+      <div className=''>
+         <div className='max-w-5xl px-5 mx-auto shadow-sm left-0 right-0 fixed border border-t-0 rounded-xl rounded-t-none  border-gray-500 '>
+            <Navbar />
+         </div>
+         <div className='h-[2000px]'></div>
+      </div>
+   );
 }
